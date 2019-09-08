c4c5e51ac7239c20241fae2040de01deca4efac7
Mathew Jennings <mathew.percussion@gmail.com>
2019-09-08 13:13:35 -0400

    Remove old point-and-click code from PlayerController.cs

--- Project Earthrise/Assets/Scripts/Control/PlayerController.cs
+++ Project Earthrise/Assets/Scripts/Control/PlayerController.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using RPG.Attributes;
+﻿using RPG.Attributes;
 using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.EventSystems;
 
 namespace RPG.Control {
   public class PlayerController : MonoBehaviour {
 
-    [SerializeField] CursorMapping[] cursorMappings = null;
     [SerializeField] float maxNavMeshProjectionDistance = 1f;
     [SerializeField] float maxNavPathLength = 40f;
-
-    [System.Serializable]
-    struct CursorMapping {
-      public CursorType cursorType;
-      public Texture2D texture;
-      public Vector2 hotspot;
-    }
 
     private Health playerHealth;
 
@@ -30,20 +19,9 @@ namespace RPG.Control {
     void Update() {
       InteractWithMovement();
 
-      //New Combat Code
       if (Input.GetKeyDown(KeyCode.E)) {
         GetComponent<Fighter>().Attack();
       }
-
-      // Old Point and click
-      if (InteractWithUI()) return;
-      if (playerHealth.IsDead()) {
-        SetCursor(CursorType.None);
-        return;
-      }
-      if (InteractWithComponent()) return;
-      if (BACKUPInteractWithMovement()) return;
-      SetCursor(CursorType.None);
     }
 
     private void InteractWithMovement() {
@@ -63,101 +41,6 @@ namespace RPG.Control {
       if (movementTarget != Vector3.zero) {
         GetComponent<Mover>().MoveInDirection(movementTarget, 1f);
       }
-    }
-
-    private bool InteractWithUI() {
-      if (EventSystem.current.IsPointerOverGameObject()) {
-        SetCursor(CursorType.UI);
-        return true;
-      }
-      return false;
-    }
-
-    private bool InteractWithComponent() {
-      RaycastHit[] hits = RaycastAllSorted();
-      foreach (RaycastHit hit in hits) {
-        IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
-        foreach (IRaycastable raycastable in raycastables) {
-          if (raycastable.HandleRaycast(this)) {
-            SetCursor(raycastable.GetCursorType());
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-
-    private RaycastHit[] RaycastAllSorted() {
-      RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-      float[] distances = new float[hits.Length];
-      for (int i = 0; i < hits.Length; i++) {
-        distances[i] = hits[i].distance;
-      }
-      Array.Sort(distances, hits);
-      return hits;
-    }
-
-    private bool BACKUPInteractWithMovement() {
-      Vector3 target;
-      bool hasHit = RaycastNavMesh(out target);
-      if (hasHit) {
-        if (Input.GetMouseButton(0)) {
-          GetComponent<Mover>().StartMoveAction(target, 1f);
-        }
-        SetCursor(CursorType.Movement);
-        return true;
-      }
-      return false;
-    }
-
-    private bool RaycastNavMesh(out Vector3 target) {
-      target = new Vector3();
-
-      RaycastHit raycastHit;
-      bool hasHit = Physics.Raycast(GetMouseRay(), out raycastHit);
-      if (!hasHit) return false;
-
-      NavMeshHit navMeshHit;
-      bool hasCastToNavMesh = NavMesh.SamplePosition(
-        raycastHit.point, out navMeshHit, maxNavMeshProjectionDistance, NavMesh.AllAreas);
-      if (!hasCastToNavMesh) return false;
-
-      target = navMeshHit.position;
-
-      NavMeshPath path = new NavMeshPath();
-      bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
-      if (!hasPath) return false;
-      if (path.status != NavMeshPathStatus.PathComplete) return false;
-      if (GetPathLength(path) > maxNavPathLength) return false;
-
-      return true;
-    }
-
-    private float GetPathLength(NavMeshPath path) {
-      float total = 0f;
-      if (path.corners.Length < 2) return total;
-      for (int i = 0; i < path.corners.Length - 1; i++) {
-        total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
-      }
-      return total;
-    }
-
-    private static Ray GetMouseRay() {
-      return Camera.main.ScreenPointToRay(Input.mousePosition);
-    }
-
-    private void SetCursor(CursorType type) {
-      CursorMapping mapping = GetCursorMapping(type);
-      Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
-    }
-
-    private CursorMapping GetCursorMapping(CursorType type) {
-      foreach (CursorMapping mapping in cursorMappings) {
-        if (mapping.cursorType == type) {
-          return mapping;
-        }
-      }
-      return cursorMappings[0];
     }
   }
 }
